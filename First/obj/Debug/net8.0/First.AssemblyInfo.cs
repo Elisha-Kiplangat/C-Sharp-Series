639b6c4005b1627cf17dd27e66b723cb38046334
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-10-13 14:32:33 +0300

    challenge prjt

--- First/obj/Debug/net8.0/First.AssemblyInfo.cs
+++ First/obj/Debug/net8.0/First.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("First")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dbe07f8b553afe3d59ff300a70a8e55468de67f")]
 [assembly: System.Reflection.AssemblyProductAttribute("First")]
 [assembly: System.Reflection.AssemblyTitleAttribute("First")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
